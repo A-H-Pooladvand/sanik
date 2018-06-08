ff448e5602be337799e229b8ace70fa2bc71aff1
Amirhossein Pooladvand <a.h.pooladvand@jahangostarpars.com>
2018-06-08 16:25:03 +0430

    Clean up home page /

--- resources/views/_layouts/front/includes/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="kopa-footer style-01">
-    <div class="container">
-        <p>© 2016 <a href="#">AsceticDesigners</a>  |  All Rights Reserved</p>
-    </div>
-</footer>
--- resources/views/main/front/index.blade.php
+++ resources/views/main/front/index.blade.php
@@ -7,37 +7,6 @@
 
     <div id="main-content">
 
-        @include('main.front._includes.slider')
-
-        {{--@include('main.front._includes.search')--}}
-
-        <section class="kopa-area kopa-area-01">
-            <div class="container">
-                <div class="widget kopa-widget-features">
-                    <div class="row">
-
-                        @include('main.front._includes.meetings')
-
-                        @include('main.front._includes.events')
-
-                    </div>
-                </div>
-            </div>
-        </section>
-
-        @include('main.front._includes.news')
-
-        @include('main.front._includes.course')
-
-
-
-        @include('main.front._includes.features')
-
-        @include('main.front._includes.audibles')
-
-
-        {{--@include('main.front._includes.sidebar-bottom')--}}
-
     </div>
 
 
