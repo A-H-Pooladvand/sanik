17a043ccdc2325dbd4546313c400804d83639435
Amirhossein Pooladvand <a.h.pooladvand@jahangostarpars.com>
2018-07-28 16:53:05 +0430

    fix

--- resources/views/project/front/index.blade.php
+++ resources/views/project/front/index.blade.php
@@ -4,44 +4,46 @@
 @section('content')
 
 
-    <div class="main">
+    <main>
+        <div class="main">
 
-        <section class="module">
-            <div class="container">
+            <section class="module">
+                <div class="container">
 
-                <div class="row multi-columns-row post-columns">
-                    @foreach($projects as $project)
-                        <div class="col-md-6 col-lg-6">
-                            <div class="post">
-                                <div class="post-thumbnail">
-                                    <a href={{ route('project.show', $project->id) }}>
-                                        <img class="img-rounded" src="{{ image_url($project->image, 55, 31, true) }}" alt="Blog-post Thumbnail"/>
-                                    </a>
-                                </div>
-                                <div class="post-header font-alt">
-                                    <h2 class="post-title">
-                                        <a href={{ route('project.show', $project->id) }}>{{ $project->title }}</a>
-                                    </h2>
-                                    <div class="post-meta">{{ $project->created_at->format('Y-d-m') }}
+                    <div class="row multi-columns-row post-columns">
+                        @foreach($projects as $project)
+                            <div class="col-md-6 col-lg-6">
+                                <div class="post">
+                                    <div class="post-thumbnail">
+                                        <a href={{ route('project.show', $project->id) }}>
+                                            <img class="img-rounded" src="{{ image_url($project->image, 55, 31, true) }}" alt="Blog-post Thumbnail"/>
+                                        </a>
                                     </div>
+                                    <div class="post-header font-alt">
+                                        <h2 class="post-title">
+                                            <a href={{ route('project.show', $project->id) }}>{{ $project->title }}</a>
+                                        </h2>
+                                        <div class="post-meta">{{ $project->created_at->format('Y-d-m') }}
+                                        </div>
+                                    </div>
+                                    <div class="post-entry">
+                                        <p>{{ str_limit($project->summary, 300) }}</p>
+                                    </div>
+                                    <div class="post-more"><a class="more-link" href={{ route('project.show', $project->id) }}>Read more</a></div>
                                 </div>
-                                <div class="post-entry">
-                                    <p>{{ str_limit($project->summary, 300) }}</p>
-                                </div>
-                                <div class="post-more"><a class="more-link" href={{ route('project.show', $project->id) }}>Read more</a></div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+
+                    </div>
+
+                    <div class="text-center">
+                        {{ $projects->links() }}
+                    </div>
 
                 </div>
+            </section>
 
-                <div class="text-center">
-                    {{ $projects->links() }}
-                </div>
-
-            </div>
-        </section>
-
-    </div>
+        </div>
+    </main>
 
 @stop
